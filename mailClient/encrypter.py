b3f8e34043985a1fb67e48376e2568486ae259a2
marwaha-raghav <marwaha.raghav@rediffmail.com>
2022-01-23 01:36:23 -0500

    fixes

---
+++ mailClient/encrypter.py
@@ -0,0 +1,46 @@
+# Steps:
+# Import Fernet
+# Then generate an encryption key, that can be used for encryption and decryption.
+# Convert the string to byte string, so that it can be encrypted.
+# Instance the Fernet class with the encryption key.
+# Then encrypt the string with Fernet instance.
+# Then it can be decrypted with Fernet class instance and it should be instanced with the same key used for encryption.
+
+from cryptography import fernet
+from fernet import Fernet
+
+
+class CryptoEngine:
+    key = Fernet.generate_key()
+
+    def __init__(self, password):
+        self.secret = password
+        # Using fernet to generate symmetrical encryption key
+
+    def encrypt_message(self):
+        """
+        This function is part of the CryptoEngine and is responsible for encrypting the secrets from the file
+        :return:
+        """
+        # instance of fernet class with key
+        encryption_engine = Fernet(self.key)
+        # password is encoded to a byte string and encrypted
+        encrypted_message = encryption_engine.encrypt(self.secret.encode())
+        # Write secret to a file
+        with open('password.txt', 'wb') as f:
+            f.write(encrypted_message)
+
+    @staticmethod
+    def decrypt_message():
+        """
+               This function is part of the CryptoEngine and is responsible for decrypting the secrets from the file
+               :return:
+               """
+        with open('password.txt', 'rb') as f:
+            read_secret = f.read()
+            decryption_engine = Fernet(CryptoEngine.key)
+            decrypted_message = decryption_engine.decrypt(read_secret)
+            return decrypted_message.decode()
+
+
+
--- mailClient/main.py
+++ mailClient/main.py
@@ -7,6 +7,7 @@ from email import encoders
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
+from encrypter import CryptoEngine
 
 
 def generate_mail():
@@ -15,14 +16,24 @@ def generate_mail():
     server.ehlo()
     server.starttls()
 
-# with open('password.txt', 'r') as f:
-    # password = f.read()
-    server.login('mvpshaggy@gmail.com', 'Password')
+    # Secret encryption and decryption
+
+    # Secret input and encryption
+    password = input("Enter your password! : ")
+    encrypter = CryptoEngine(password)
+    encrypter.encrypt_message()
+
+    # Secret decryption
+    decrypter = CryptoEngine(password)
+    credential_password = decrypter.decrypt_message()
+
+    # generate email
+    server.login('mvpshaggy@gmail.com', credential_password)
     mail_msg = MIMEMultipart()
     mail_msg['From'] = 'biostar'
     mail_msg['To'] = 'raghavmarwaha1998@gmail.com'
     mail_msg['Subject'] = "Test email from smtp client"
-
+    # write body to file
     with open('message.txt', 'r') as bod:
         message_body = bod.read()
     mail_msg.attach(MIMEText(message_body, 'plain'))
@@ -30,17 +41,21 @@ def generate_mail():
 # functionality for attachments
     file = 'keylogger.PNG'
     attachment = open(file, 'rb')
-
+# generate payload for attachment
     payload = MIMEBase('image', 'png')
     payload.set_payload(attachment.read())
 
+# encode payload and attach with header and body MIME
     encoders.encode_base64(payload)
     payload.add_header('Content-Disposition', 'attachment', filename=file)
     mail_msg.attach(payload)
 
+# convert the message to string and send email
     text = mail_msg.as_string()
     server.sendmail('mvpshaggy@gmail.com', 'raghavmarwaha1998@gmail.com', text)
     attachment.close()
+
+# function call
 
 
 generate_mail()
