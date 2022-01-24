18c72a402ec1b1e453f1b8cc7cfd083a37a5cf3e
marwaha-raghav <marwaha.raghav@rediffmail.com>
2022-01-24 15:16:36 -0500

    updated basicTCPClient with chat multi threaded server and multi-client chat feature

--- basicTCPClient/client.py
+++ basicTCPClient/client.py
@@ -1,10 +1,45 @@
 import socket
+import threading
 
 HOST = '192.168.0.16'
 port = 9090
 
-socket  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((HOST,port))
+nickname = input("Enter your nickname: ")
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect((HOST, port))
 
 socket.send("Hello server!".encode())
 print(socket.recv(1024).decode())
+
+
+def receive():
+    while True:
+        try:
+            message  = socket.recv(1024).decode('ascii')
+            if message == 'NICK':
+                socket.send(nickname.encode('ascii'))
+            else:
+                print(messasge)
+
+        except:
+            print("An error occured")
+            socket.close()
+            break
+
+
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+        socket.send(message.endode('ascii'))
+
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
+
+
+
+
+
--- basicTCPClient/server.py
+++ basicTCPClient/server.py
@@ -1,6 +1,7 @@
 # TCP server
 
 import socket
+import threading
 
 HOST = '192.168.0.16'
 port = 9090
@@ -13,10 +14,55 @@ server_socket.bind((HOST, port))
 
 server_socket.listen(5)
 
-while True:
-    communication_socket, address = server_socket.accept()
-    print(f"Connected to {address}")
-    message = communication_socket.recv(1024).decode()
-    print(f"Message from the client is: {message}")
-    communication_socket.send(f"Got your message".encode())
+clients = []
+nicknames = []
+
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} left the chat!'.encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+
+def receive():
+    while True:
+        communication_socket, address = server_socket.accept()
+        print(f"Connected to {address}")
+        communication_socket.send('NICK'.encode('ascii'))
+        # message = communication_socket.recv(1024).decode()
+        # print(f"Message from the client is: {message}")
+        # communication_socket.send(f"Got your message".encode())
+        nickname = communication_socket.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(communication_socket)
+
+        print(f"Nickname of the client is {nickname}")
+        broadcast(f"{nickname} joined the server.")
+        communication_socket.send("Connected to the server".encode())
+
+        thread = threading.Thread(target=handle, args=(communication_socket,))
+        thread.start()
+
+
+print("Server Listening")
+receive()
+
+
+
+
+
 
